92254fb3245c4f1fb4341353fe64a52cca6435cd
BrunoGeorgevich <brunogeorgevich@hotmail.com>
2016-03-21 21:58:14 -0300

    Functions updated

--- functions.js
+++ functions.js
@@ -1,3 +1,6 @@
 function hpercent(p,ref) { return (ref.height/100)*p }
 function wpercent(p,ref) { return (ref.width/100)*p }
+function minorThan(a,b) { return (a<b) ? a : b; }
+function majorThan(a,b) { return (a>b) ? a : b; }
+function equals(a,b) { return a==b; }
 
